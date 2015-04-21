64480b60e5446dde3f807658289970f3c044db6f
hjs6877 <hjs6877@gmail.com>
2015-04-21 16:56:24 +0900

    sdgf

--- Algorthim_version20150420/src/kr/ac/korea/test/sort/insertion/InsertionSort1Test.java
+++ Algorthim_version20150420/src/kr/ac/korea/test/sort/insertion/InsertionSort1Test.java
@@ -1,6 +1,9 @@
 package kr.ac.korea.test.sort.insertion;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 import kr.ac.korea.sort.insertion.InsertionSort1;
 
 import org.junit.BeforeClass;
@@ -8,11 +11,17 @@ import org.junit.Test;
 
 public class InsertionSort1Test {
 	private static int[] num = {2, 5, 1, 10, 9, 3, 8, 6, 7, 4};
+	private static int[] B;
 	private static InsertionSort1 is;
 	
 	@BeforeClass
 	public static void init(){
+		
 		is = new InsertionSort1(num);
+		
+		B = new int[num.length];
+		System.arraycopy(num, 0, B, 0, 10);
+		Arrays.sort(B);
 	}
 	
 	@Test
@@ -20,4 +29,9 @@ public class InsertionSort1Test {
 		assertNotNull(is);
 	}
 
+	@Test
+	public void sortTest(){
+		is.sort();
+		assertArrayEquals(num, B);
+	}
 }
