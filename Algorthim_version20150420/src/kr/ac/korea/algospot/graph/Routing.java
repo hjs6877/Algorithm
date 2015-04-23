cb0193be4b66c47e7d20cf581e7c97d83ed65ef7
hjs6877 <hjs6877@gmail.com>
2015-04-23 19:09:46 +0900

    ㅎㅎㅎㅎ

--- Algorthim_version20150420/src/kr/ac/korea/algospot/graph/Routing.java
+++ Algorthim_version20150420/src/kr/ac/korea/algospot/graph/Routing.java
@@ -46,11 +46,14 @@ import java.util.Scanner;
  * }
  */
 public class Routing {
+	/**
+	 * ############# 가중치에 log 씌우는 작업부터 해야됨. ##################
+	 */
+	public static Map<Integer, List<Map<String, Object>>> graphMap;
 	
-	public static Map<Integer, List<Map<Integer, List<Double>>>> graphMap;
-	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args){
-		graphMap = new HashMap<Integer, List<Map<Integer, List<Double>>>>();
+		graphMap = new HashMap<Integer, List<Map<String, Object>>>();
 		Scanner scanner1 = new Scanner(System.in);
 		int executeCount = scanner1.nextInt();
 		for(int i=0; i<executeCount; i++){
@@ -66,8 +69,10 @@ public class Routing {
 			/**
 			 * 다익스트라 알고리즘을 통한 최소 노이즈 경로의 노이즈 값 출력....
 			 */
+			System.out.println();
 			System.out.println("시작정점 0에 부수된 정점 개수: " + graphMap.get(0).size());
 			System.out.println("시작정점 0에 부수된 정점 목록: " + graphMap.get(0));
+			System.out.println();
 			
 			// 시작정점에서 해당 정점까지의 가장 짧은 거리가 저장 된 배열.
 			double[] dist = new double[Integer.parseInt(veInfos[0])];
@@ -90,9 +95,22 @@ public class Routing {
 					continue;
 				}
 				
-				List<Map<Integer, List<Double>>> adjVList = graphMap.get(no);
-				for(Map<Integer, List<Double>> adjVMap : adjVList){
-					Iterator<Integer> keyIter = adjVMap.keySet().iterator();
+				List<Map<String, Object>> adjVList = graphMap.get(no);
+				for(Map<String, Object> adjVMap : adjVList){
+					int adjVNo = (int) adjVMap.get("no");
+					List<Double> adjVWeightList = (List<Double>) adjVMap.get("weightList");
+					System.out.print(no + "에 인접한 정점: [" + adjVNo + ":  " + adjVWeightList + "], ");
+					
+					// 연결된 정점에 다중 간선이 존재할 수 있음.
+					for(int j=0; j < adjVWeightList.size(); j++){
+						double adjVCost = cost + adjVWeightList.get(j);
+						// 더 짧은 경로를 발견하면, dist[ ]를 갱신하고 우선순위 큐에 넣는다.
+						if(dist[adjVNo] > adjVCost){
+							dist[adjVNo] = adjVCost;
+							Vertex vertex = new Vertex(adjVNo, adjVCost);
+							pq.add(vertex);														// 큐 내부적으로 거리가 작은놈이 우선 순위가 높으므로 먼저 나옴.
+						}
+					}
 				}
 			}
 		}
@@ -106,9 +124,20 @@ public class Routing {
 	 * - 정점의 번호는 vCount보다 작아야한다.
 	 * - 간선의 갯수 만큼만 루프를 돈다.
 	 * 
+	 * 
+	 * ====== 그래프 생성 프로세스 ====
+	 * graphMap(출발정점, 도착정점의 목록 List<Map<도착정점, 가중치 List<가중치>>>);
+	 * 1.맵에 저장된 출발정점이 존재하는지 확인.
+	 * 		1-1. 존재한다면, 출발정점을 키로 도착정점의 목록을 가져온다.
+	 * 			1-1-1. 목록을 루프 돌면서 도착 정점이 존재하는지 확인.
+	 * 				1-1-1-1. 도착 정점이 존재한다면 도착 정점을 키로 가중치 List를 가져온 후, 목록에 가중치를 추가한다.
+	 * 			1-1-2. 도착 정점이 존재하지 않는다면 도착 정점을 키로하고 가중치 List를 생성해서 가중치를 추가한다.
+	 * 		1-2. 존재하지 않는다면, 
+	 * 			1-2-1. 출발정점을 키로, 도착정점의 List를 생성하고, 도착 정점의 Map을 생성하여 가중치 List를 생성 한 후, 가중치를 추가한다.
 	 * @param vCount
 	 * @param eCount
 	 */
+	@SuppressWarnings("unchecked")
 	public static void createGraph(int vCount, int eCount){
 		for(int i=0; i < eCount; i++){
 			Scanner scanner = new Scanner(System.in);
@@ -120,27 +149,27 @@ public class Routing {
 			double weight = Double.parseDouble(infos[2]);
 			
 			if(Routing.isValidVertex(vCount, startV) && Routing.isValidVertex(vCount, endV)){
-				List<Map<Integer, List<Double>>> endVList = null;
-				Map<Integer, List<Double>> endVMap = null;
+				List<Map<String, Object>> endVList = null;
+				Map<String, Object> endVMap = null;
 				List<Double> weightList = null;
 				
 				/**
 				 * 그래프 생성.
-				 * 시작 정점이 존재 할때와 존재하지 않을때로 구분해서 처리.
+				 * 시작 정점이 존재 할 때와 존재하지 않을 때로 구분해서 처리.
 				 */
 				if(graphMap.containsKey(startV)){
 					endVList = graphMap.get(startV);
 					boolean isExistEndV = false;
 					
-					for(Map<Integer, List<Double>> map : endVList){
+					for(Map<String, Object> map : endVList){
 						/**
 						 * - 끝 정점이 존재한다면 해당 정점의 가중치 리스트를 가져와서 가중치 추가.
 						 */
-						if(map.containsKey(endV)){
+						if((Integer)map.get("no") == endV){
 							isExistEndV = true;
-							weightList = map.get(endV);
+							weightList = (List<Double>) map.get("weightList");
 							weightList.add(weight);
-							System.out.println(startV + "(기존 시작정점)" + " -> " + endV + "(기존 끝정점)" + "에 가중치 추가");
+							System.out.println(startV + "(기존 시작정점)" + " -> " + (Integer)map.get("no") + "(기존 끝정점)" + "에 가중치 추가");
 							break;
 						}
 					}
@@ -149,11 +178,12 @@ public class Routing {
 					 * - 끝 정점이 존재하지 않는다면 끝 정점 생성, 끝 정점에 간선 가중치 값 추가한 후, 끝 정점 추가.
 					 */
 					if(!isExistEndV){
-						endVMap = new HashMap<Integer, List<Double>>();
+						endVMap = new HashMap<String, Object>();
 						weightList = new ArrayList<Double>();
 						
 						weightList.add(weight);
-						endVMap.put(endV, weightList);
+						endVMap.put("no", endV);
+						endVMap.put("weightList", weightList);
 						
 						endVList.add(endVMap);
 						System.out.println(startV + "(기존 시작정점)" + " -> " + endV + "(새로운 끝 정점)" + "에 가중치 추가");
@@ -161,12 +191,13 @@ public class Routing {
 					
 					
 				}else{
-					endVList = new ArrayList<Map<Integer, List<Double>>>();
-					endVMap = new HashMap<Integer, List<Double>>();
+					endVList = new ArrayList<Map<String, Object>>();
+					endVMap = new HashMap<String, Object>();
 					weightList = new ArrayList<Double>();
 					
 					weightList.add(weight);
-					endVMap.put(endV, weightList);
+					endVMap.put("no", endV);
+					endVMap.put("weightList", weightList);
 					endVList.add(endVMap);
 					 
 					graphMap.put(startV, endVList);
